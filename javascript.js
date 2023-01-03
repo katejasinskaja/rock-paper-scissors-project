22540c4774c61515be9162a0185bcca6e834ac40
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2023-01-03 21:34:47 +0300

    Add the function  that will turn input into a number

--- javascript.js
+++ javascript.js
@@ -13,4 +13,22 @@ function getComputerChoice () {
             break; 
     }
 }
-console.log (getComputerChoice());
+
+
+function playRound (playerSelection, computerSelection) {
+playerSelection.toLowerCase = playerSelection.toUpperCase;
+switch (randomNumber) {
+     case 0:
+        console.log ("It's a draw");
+        break;
+    case 1:
+        console.log ("You lose! Paper beats Rock");
+        break;
+    case 2:
+        console.log ("You win! Rock beats Scissors");
+        break;
+}
+}
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice ();
+console.log (playRound(playerSelection, computerSelection));
